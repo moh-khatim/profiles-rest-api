3b1a251d77b7388e909eec547771154c860fcf20
Mohammed Abdalla <moh.khatim.dev@gmail.com>
2023-11-07 00:21:07 +0400

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,11 +3,14 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from rest_framework import filters
 
 from profiles_api import serializers
 from profiles_api import models
 from profiles_api import permissions
+
 
 class HelloAPIView(APIView):
 
@@ -48,3 +51,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,filters.OrderingFilter,)
     search_fields = ('name', 'email')
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
