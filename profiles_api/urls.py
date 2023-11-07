52813613b9048c0d0504c78599530a4dfa49ca39
Mohammed Abdalla <moh.khatim.dev@gmail.com>
2023-11-07 17:57:58 +0400

    Added profile feed API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('profiles', views.UserProfileViewSet)
+router.register('feeds', views.ProfileFeedItemViewSet)
 
 
 urlpatterns = [
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.settings import api_settings
 from rest_framework import filters
 
@@ -54,3 +55,13 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
 class UserLoginApiView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ProfileFeedItemViewSet(viewsets.ModelViewSet):
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        return serializer.save(user_profile=self.request.user)
